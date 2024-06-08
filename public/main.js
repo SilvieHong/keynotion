1ab3005ad0ffc81cb5462214e6117c14cd1c6c40
SilvieHong <heh9007@gmail.com>
2024-06-08 10:03:11 +0900

    데이터 하나씩 쪼개서 가져오기 + 답안보기 기능 구현

--- public/main.js
+++ public/main.js
@@ -1,25 +1,47 @@
 const interviewEl = document.querySelector("#interviews");
+const div = document.getElementById("data-display");
 
-const getDatabasesFromBackend = async () => {
-  const res = await fetch("http://localhost:3000/interview");
+// JSON 데이터를 불러오기
+async function getDatabasesFromBackend() {
+  const res = await fetch("http://localhost:3000/interviews");
   const data = await res.json();
   return data;
-};
+}
 
-const addInterviewToDom = async () => {
-  const interviews = await getDatabasesFromBackend();
+// 데이터를 하나씩 보여줄 인덱스를 저장하는 변수
+let currentIndex = 0;
+let jsonData = [];
 
-  /*
-  1개씩 보여주도록 수정 필요
-  */
-  interviews.forEach((interview) => {
-    const div = document.createElement("div");
-    div.claseName = "test";
-    div.innerHTML = `
-    <h3>${interview.title}</h3>
+// 다음 클릭 이벤트 핸들러
+document.getElementById("next-btn").addEventListener("click", () => {
+  if (currentIndex < jsonData.length) {
+    displayQuestion(jsonData[currentIndex]);
+    currentIndex++;
+  } else {
+    alert("No more items to display");
+  }
+});
+
+// 답안 클릭 이벤트 핸들러
+document.getElementById("answer-btn").addEventListener("click", () => {
+  currentIndex--;
+  displayAnswer(jsonData[currentIndex]);
+  currentIndex++;
+});
+
+function displayAnswer(data) {
+  div.innerHTML = `
+    ${JSON.stringify(data.content)}
     `;
-    interviewEl.appendChild(div);
-  });
-};
+}
 
-addInterviewToDom();
+// 데이터를 화면에 표시하는 함수
+function displayQuestion(data) {
+  div.innerHTML = `
+    ${JSON.stringify(data.title)}
+    `;
+}
+
+window.onload = async () => {
+  jsonData = await getDatabasesFromBackend();
+};
